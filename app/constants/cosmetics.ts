2a54d683f18c2c123bf6f06206a1b7725770e061
A-dith-ya <awedithya@gmail.com>
2025-05-29 20:10:11 -0700

    Add COSMETICS constant with sticker definitions

---
+++ app/constants/cosmetics.ts
@@ -0,0 +1,81 @@
+import { Cosmetic } from "@/app/types/storeTypes";
+
+export const COSMETICS: Cosmetic[] = [
+  // Astrological Stickers
+  {
+    id: "star-explorer",
+    category: "STICKER",
+    price: 100,
+    image: require("@/assets/images/cosmetics/star-explorer.webp"),
+  },
+  {
+    id: "rocket-blast",
+    category: "STICKER",
+    price: 300,
+    image: require("@/assets/images/cosmetics/rocket-blast.webp"),
+  },
+  // Biology Stickers
+  {
+    id: "double-helix",
+    category: "STICKER",
+    price: 300,
+    image: require("@/assets/images/cosmetics/double-helix.webp"),
+  },
+  {
+    id: "mini-cell",
+    category: "STICKER",
+    price: 200,
+    image: require("@/assets/images/cosmetics/mini-cell.webp"),
+  },
+  {
+    id: "lab-flask",
+    category: "STICKER",
+    price: 200,
+    image: require("@/assets/images/cosmetics/lab-flask.webp"),
+  },
+  {
+    id: "green-beaker",
+    category: "STICKER",
+    price: 100,
+    image: require("@/assets/images/cosmetics/green-beaker.webp"),
+  },
+  // Planet Stickers
+  {
+    id: "earth",
+    category: "STICKER",
+    price: 200,
+    image: require("@/assets/images/cosmetics/earth.webp"),
+  },
+  {
+    id: "saturn",
+    category: "STICKER",
+    price: 200,
+    image: require("@/assets/images/cosmetics/saturn.webp"),
+  },
+  {
+    id: "moonlit-cat",
+    category: "STICKER",
+    price: 300,
+    image: require("@/assets/images/cosmetics/moonlit-cat.webp"),
+  },
+  // Plant Stickers
+  {
+    id: "bonsai",
+    category: "STICKER",
+    price: 200,
+    image: require("@/assets/images/cosmetics/bonsai.webp"),
+  },
+  // Animal Stickers
+  {
+    id: "dream-unicorn",
+    category: "STICKER",
+    price: 200,
+    image: require("@/assets/images/cosmetics/dream-unicorn.webp"),
+  },
+  {
+    id: "happy-corgi",
+    category: "STICKER",
+    price: 300,
+    image: require("@/assets/images/cosmetics/happy-corgi.webp"),
+  },
+];
