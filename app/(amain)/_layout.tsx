5c4048a7dcc136f710a6fb16d50af5bc85e4d110
A-dith-ya <awedithya@gmail.com>
2025-06-02 15:31:59 -0700

    Enhance tab bar styling in MainLayout with border properties

--- app/(amain)/_layout.tsx
+++ app/(amain)/_layout.tsx
@@ -9,6 +9,10 @@ export default function MainLayout() {
         tabBarActiveTintColor: COLORS.primary,
         tabBarInactiveTintColor: COLORS.darkGray,
         headerShown: false,
+        tabBarStyle: {
+          borderTopWidth: 1,
+          borderTopColor: COLORS.mediumGray,
+        },
       }}
     >
       <Tabs.Screen
